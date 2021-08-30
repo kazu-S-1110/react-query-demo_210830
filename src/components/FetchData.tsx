691be204f318b651521704cb9f8e7636d13581ca
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-30 13:48:27 +0900

    環境構築が完了。ベースとなるものも完成。

---
+++ src/components/FetchData.tsx
@@ -0,0 +1,16 @@
+import { Text } from '@chakra-ui/react';
+
+export const FetchData = () => {
+  return (
+    <>
+      <Text
+        bgGradient="linear(to-l, #7928CA,#FF0080)"
+        bgClip="text"
+        fontSize="6xl"
+        fontWeight="extrabold"
+      >
+        I'm testing FetchData
+      </Text>
+    </>
+  );
+};
